863ee6aa0b6d5fca648e88e2536f295dba01ee7c
Fabian Romero Sotelo <astramex19@gmail.com>
2018-10-18 02:16:49 -0500

    Búsqueda de productos anexada a un objeto

--- Ejercicio/ViewController.swift
+++ Ejercicio/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var productos = [[String: Any]]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://www.liverpool.com.mx/tienda/?s=zoo&d3106047a194921c01969dfdec083925=json").responseJSON { (response) in
+           
+            
+            if let json = response.result.value as! [String: Any]? {
+                
+                if let json2 = json["contents"] as! [[String: Any]]? {
+                    
+                    for val in json2 {
+                        
+                        if let json3 = val["mainContent"] as! [[String: Any]]? {
+                            
+                            for val2 in json3 {
+                                
+                                if let json4 = val2["contents"] as! [[String: Any]]? {
+                                
+                                    for val3 in json4 {
+                                        
+                                        if let json5 = val3["records"] as! [[String: Any]]? {
+                                          
+                                            for val4 in json5 {
+                                                if let json6 = val4["records"] as! [[String: Any]]? {
+                                                    
+                                                    self.productos = json6
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+
+                }
+                
+            }
+            
+        }
     }
 
 
