cdbd6bec04f1cfdaf3792d954214d18c9b4d72bc
Fabian Romero Sotelo <astramex19@gmail.com>
2018-10-18 00:37:08 -0500

    Initial Commit

---
+++ Ejercicio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejercicio
+//
+//  Created by Fabian Romero Sotelo on 10/18/18.
+//  Copyright © 2018 Fabian Romero Sotelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
