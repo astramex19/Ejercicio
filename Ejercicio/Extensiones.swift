87f6845f4aa95544e051d455c69111fcc0ccd0d7
Fabian Romero Sotelo <astramex19@gmail.com>
2018-10-18 05:17:05 -0500

    Se agrego un Framework para mejorar el listado de las imágenes

---
+++ Ejercicio/Extensiones.swift
@@ -0,0 +1,28 @@
+//
+//  Extensiones.swift
+//  Ejercicio
+//
+//  Created by Fabian Romero Sotelo on 10/18/18.
+//  Copyright © 2018 Fabian Romero Sotelo. All rights reserved.
+//
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
--- Ejercicio/ViewController.swift
+++ Ejercicio/ViewController.swift
@@ -60,7 +60,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelega
         
         if let dictionnary : NSDictionary = attributes as NSDictionary? {
             if let newArr : NSArray = dictionnary.object(forKey: "sku.thumbnailImage") as? NSArray {
-                cell.imagen.downloaded(from: (newArr.firstObject as! String))
+                let imageView = cell.viewWithTag(1) as! UIImageView                
+                imageView.sd_setImage(with: URL(string: (newArr.firstObject as! String)))
+                
             }
         }
         return cell
@@ -138,24 +140,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelega
 }
 
 
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-            }
-            }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
-}
+
 
