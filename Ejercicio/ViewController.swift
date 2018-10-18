da31315e587a000fc5e1f9b47be50b5ee9573ab4
Fabian Romero Sotelo <astramex19@gmail.com>
2018-10-18 04:56:41 -0500

    boton de búsqueda agregado

--- Ejercicio/ViewController.swift
+++ Ejercicio/ViewController.swift
@@ -9,63 +9,21 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelegate {
     
+    @IBOutlet weak var textfieldBuscar: UITextField!
     
     
     @IBOutlet weak var tableView: UITableView!
     
     var productos = [[String: Any]]()
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Alamofire.request("https://www.liverpool.com.mx/tienda/?s=zoo&d3106047a194921c01969dfdec083925=json").responseJSON { (response) in
-           
-            
-            if let json = response.result.value as! [String: Any]? {
-                
-                if let json2 = json["contents"] as! [[String: Any]]? {
-                    
-                    for val in json2 {
-                        
-                        if let json3 = val["mainContent"] as! [[String: Any]]? {
-                            
-                            for val2 in json3 {
-                                
-                                if let json4 = val2["contents"] as! [[String: Any]]? {
-                                
-                                    for val3 in json4 {
-                                        
-                                        if let json5 = val3["records"] as! [[String: Any]]? {
-                                          
-                                            for val4 in json5 {
-                                                if let json6 = val4["records"] as! [[String: Any]]? {
-                                                    
-                                                    self.productos.append(json6[0])
-                                                   
-                                                    
-                                                   
-                                                }
-                                                
-                                            }
-                                            DispatchQueue.main.async {
-                                                self.tableView.reloadData()
-                                            }
-                                        }
-                                    }
-                                }
-
-                            }
-                        }
-                    }
-
-                }
-                
-            }
-            
-        }
-       
+        
     }
     
     // MARK: - delegados de la tabla
@@ -84,19 +42,19 @@ class ViewController: UIViewController, UITableViewDataSource {
 
         if let dictionnary : NSDictionary = attributes as NSDictionary? {
             if let newArr : NSArray = dictionnary.object(forKey: "product.description") as? NSArray {
-                cell.titulo.text = (newArr.firstObject as! String)
+                cell.titulo.text =  (newArr.firstObject as! String)
             }
         }
         
         if let dictionnary : NSDictionary = attributes as NSDictionary? {
             if let newArr : NSArray = dictionnary.object(forKey: "sku.list_Price") as? NSArray {
-                cell.precio.text = (newArr.firstObject as! String)
+                cell.precio.text = "Precio $" + (newArr.firstObject as! String)
             }
         }
         
         if let dictionnary : NSDictionary = attributes as NSDictionary? {
             if let newArr : NSArray = dictionnary.object(forKey: "department") as? NSArray {
-                cell.ubicacion.text = (newArr.firstObject as! String)
+                cell.ubicacion.text = "Departamento " + (newArr.firstObject as! String)
             }
         }
         
@@ -105,16 +63,79 @@ class ViewController: UIViewController, UITableViewDataSource {
                 cell.imagen.downloaded(from: (newArr.firstObject as! String))
             }
         }
-        
-        
-        
-       
         return cell
         
     }
-
+    
+    func textFieldShouldReturn(_ scoreText: UITextField) -> Bool {
+        self.view.endEditing(true)
+        
+        guard let text = textfieldBuscar.text, !text.isEmpty else {
+            return false
+        }
+        buscar(value: textfieldBuscar.text!)
+        
+        return false
+    }
+    
+    @IBAction func botonBuscar(_ sender: Any) {
+        
+        guard let text = textfieldBuscar.text, !text.isEmpty else {
+            return
+        }
+        buscar(value: textfieldBuscar.text!)
+    }
+    
+    
+    func buscar(value:String){
+        
+        Alamofire.request("https://www.liverpool.com.mx/tienda/?s=\(value)&d3106047a194921c01969dfdec083925=json").responseJSON { (response) in
+            
+            
+            if let json = response.result.value as! [String: Any]? {
+                
+                if let json2 = json["contents"] as! [[String: Any]]? {
+                    
+                    for val in json2 {
+                        
+                        if let json3 = val["mainContent"] as! [[String: Any]]? {
+                            
+                            for val2 in json3 {
+                                
+                                if let json4 = val2["contents"] as! [[String: Any]]? {
+                                    
+                                    for val3 in json4 {
+                                        
+                                        if let json5 = val3["records"] as! [[String: Any]]? {
+                                            
+                                            for val4 in json5 {
+                                                if let json6 = val4["records"] as! [[String: Any]]? {
+                                                    
+                                                    self.productos.append(json6[0])
+                                                }
+                                                
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                    
+                }
+                
+            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+    }
+    
 }
-
 
 
 extension UIImageView {
@@ -137,3 +158,4 @@ extension UIImageView {
         downloaded(from: url, contentMode: mode)
     }
 }
+
